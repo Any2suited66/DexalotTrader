e1e6efd412fa57493a493abd5de1e009fcdbdd85
Tyler Hackett <thackett86181@gmail.com>
2024-09-03 21:21:15 -0700

    implement recconnection

--- sendOrder.ts
+++ sendOrder.ts
@@ -1,8 +1,16 @@
 import {ethers} from 'ethers';
 import {config} from 'dotenv';
 import WebSocket from 'ws';
+import {Parser} from '@json2csv/plainjs';
+
+const csv = require('csv')
 const binaryToString = require('binary-string');
 const fs = require('fs');
+// const { createCsvWriter } = require('csv-writer');
+const path = require("path");
+const { parse } = require("csv-parse");
+
+
 
 
 const axios = require('axios').default;
@@ -49,7 +57,6 @@ async function getFirmQuote(amount: string, side: string) {
     }
   }).then((response: { data: any; }) => {
     responseData = response.data
-    console.log(responseData)
   }).catch(function(error: any) {
     console.log(error)
     return error
@@ -77,32 +84,99 @@ async function sendOrder(amount: string, side: string) {
   }
 }
 
+// A helper to search for values ​​in files =D
+const findWord = async (text: string, filePath: any) => {
+  const result = await fs.readFileSync(filePath);
+  return Promise.resolve(RegExp("\\b" + text + "\\b").test(result));
+};
+
+async function write(filename: string, price: any, timestamp: any) {
+  var appendThis = [{
+    timestamp,
+    price,
+  }]
+  var fields = ['timestamp', 'price'];
+
+  var newLine = "\r\n";
+  fs.stat(filename, function (err: null) {
+    const parser = new Parser({ fields, header: false });
+    
+    if (err == null) {
+  
+      //write the actual data and end with newline
+      var csv = parser.parse(appendThis) + newLine;
+  
+      fs.appendFile(filename, csv, function (err: any) {
+        if (err) throw err;
+      });
+    } else {
+      //write the headers and newline
+  
+      fs.writeFile(filename, newLine, function (err: any) {
+        if (err) throw err;
+      });
+    }
+  });
+}
+
 function averageLastN(numbers: any, n: number) {
   const slicedArray = numbers.slice(-n);
   const sum = slicedArray.reduce((acc: any, current: any) => acc + current, 0);
   return sum / slicedArray.length;
 }
 
-async function processMsgQue(msgQue: any, ws: any, msgData: any) {
-  while (msgQue.length > 0) {
-    const msg = msgQue.shift();
-    const interval = 200
+async function getTimestampAndPriceData(filename: string, data: any[] = []): Promise<[]> {
+  return new Promise((reject) => {
+    fs.createReadStream(filename)
+      .pipe(parse({ delemiter: ',', columns: true, ltrim: true }))
+      .on('data', function(row: any) {
+        data.push(row);
+      })
+      .on('error', function(err: any) {
+        console.log(err);
+        reject(err);
+      })
+      .on('end', function() {
+        const timeStampData = data.map((priceData: any) => priceData.timeStamp)
+        const priceData = data.map((priceData: any) => priceData.price)
+        return {timeStampData, priceData};
+      });
+      
+  });
+}
+
+async function processMsgQue(msgData: any) {
+    const interval = 202
     timeStamp = Date.now()
-    const msgStr = JSON.parse(binaryToString.fromBuffer(msgData))
+    const msgStr = await JSON.parse(binaryToString.fromBuffer(msgData))
+  
     msgStr['timestamp'] = timeStamp
-    const avaxPrice = msgStr.data['baseinUsd']
+    var avaxPrice = msgStr.data['baseinUsd']
+
     // Adding a timestamp to the price data so it's easier to filter out duplicates. Dexalot sends the same data twice every 6 seconds.
     const timeStampData = msgStr['timestamp']
-    const priceDataObject = {avaxPrice, timeStampData}
+
     if (avaxPrice !== undefined) {
-      priceDataArray.push(priceDataObject)
+      await write("priceData.csv", avaxPrice, timeStampData)    
     }
-    const uniquePriceDataArray = priceDataArray.filter((obj, index) => priceDataArray.findIndex((item) => item.timeStampData === obj.timeStampData) === index)
-    if (priceDataArray.length > interval) {
+
+    let timeStampAndPriceDataArrays: string[] = await getTimestampAndPriceData('priceData.csv');
+    var priceDataArray = timeStampAndPriceDataArrays[1]
+    var timeStampDataArray = timeStampAndPriceDataArrays[0]
+
+    
+    // Sometimes we get messages with no price data. We don't want to write these to the CSV. We also don't want to write duplicate data so I check if the last line in the CSV contains the current timestamp.
+    const uniquePriceArray: string[] = []
+    for (const item of priceDataArray) {
+      if (!uniquePriceArray.includes(item)) {
+        uniquePriceArray.push(item)
+      }
+    }
+    if (uniquePriceArray.length > interval) {
       const avaxBalance = Number(await getAvaxBalance())
       const usdcBalance = Number(await getUsdcBalance())
-      const last10Minutes = uniquePriceDataArray.map((priceData: any) => priceData.avaxPrice).slice(-100)
-      const last20Minutes = uniquePriceDataArray.map((priceData: any) => priceData.avaxPrice).slice(-200)
+      const last10Minutes = priceDataArray.slice(-100)
+      const last20Minutes = priceDataArray.slice(-200)
       const sma10 = averageLastN(last10Minutes, 100)
       const sma20 = averageLastN(last20Minutes, 200)
 
@@ -122,14 +196,51 @@ async function processMsgQue(msgQue: any, ws: any, msgData: any) {
     }
   }
   }
+  
+
+// export interface CandleDataRaw {
+//   open: string
+//   close: string
+//   high: string
+//   low: string
+//   change: string
+//   date: string
+//   volume: string
+// }
+
+// export interface WsRawChartSnapshot {
+//   data: CandleDataRaw[] | CandleDataRaw
+//   type: string
+//   pair: string
+// }
+
+// function chartSubscribe(charttype: string) {
+//   const socket = new WebSocket('wss://api.dexalot.com')
+
+//   const msg: any = {
+//       data: "AVAX/USDC",
+//       pair: "AVAX/USDC",
+//       chart: charttype,
+//       type: "chartsubscribe",
+//     }
+
+  
+//     socket.send(JSON.stringify(msg))
+// }
+
+let lastActivity = Date.now()
+setInterval(checkActivity, 10000)
+
+function checkActivity() {
+  const currentTime = Date.now()
+  if (currentTime - lastActivity > 30000) {
+    console.log('No activity for 30 seconds. Closing connection.')
+    socketCloseListener()
   }
+  lastActivity = currentTime
+}
 
-
-let priceDataArray: any[] = [];
-const ws = new WebSocket('wss://api.dexalot.com')
-let timeStamp 
-
-ws.onopen = () => {
+const socketOpenListener = () => {
   const msg = {
     data: 'AVAX',
     pair: 'AVAX/USDC',
@@ -137,25 +248,28 @@ ws.onopen = () => {
     type: 'subscribe'
   }
   ws.send(JSON.stringify(msg))
-  console.log('Connected to the server')
-}
+  console.log('Connected to the server')  
+};
 
-ws.addEventListener('message', async function(event) {
+const messageListener = async (event: { data: any; }) => {
   const msgData = event.data
   const msgQue: WebSocket.Data[] = []
   msgQue.push(msgData)
-  setTimeout(async () => {
-    await processMsgQue(msgQue, ws, msgData)
-  }
-)
-})
-
-ws.onclose = () => {
-  console.log('Connection closed')
-  ws.close()
+  await processMsgQue(msgData)
 }
 
-ws.onerror = (error) => {
-  console.log('Connection closed on error')
-  console.log(error)
-}
+const socketCloseListener = () => {
+  // Reconnect attempt
+  const ws = new WebSocket('wss://api.dexalot.com');
+  ws.addEventListener('open', socketOpenListener);
+  ws.addEventListener('message', messageListener);
+  ws.addEventListener('close', socketCloseListener);
+};
+
+var ws = new WebSocket('wss://api.dexalot.com')
+let timeStamp: number 
+
+ws = new WebSocket('wss://api.dexalot.com')
+ws.addEventListener('open', socketOpenListener);
+ws.addEventListener('message', messageListener);
+ws.addEventListener('close', socketCloseListener)
